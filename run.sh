496b1e7a6523f470d93a6e3e43785b4e495d6120
denispalnitsky <denis.palnitsky@aurea.com>
2021-07-27 11:47:49 +0300

    fix scripts

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/bin/sh -l
+set -e
+
 git config --global user.email "gitops-release@github.com"
 git config --global user.name "Gitops Release User"
 git clone https://$4@github.com/$5.git  infra-as-code-repo
-cd  infra-as-code-repo
-python replace-key.py --file $1 --key $2 --value $3
+python replace-key.py --file infra-as-code-repo/$1 --key $2 --value $3
 git add .
 git commit -m"Release of key $2 in $1"
 git push
