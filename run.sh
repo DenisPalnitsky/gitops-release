bd3ae122da82985ed981a3707a932ca73a81cfbf
Gitops Release User <gitops-release@github.com>
2021-07-27 12:11:35 +0300

    get replace script

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ set -e
 git config --global user.email "gitops-release@github.com"
 git config --global user.name "Gitops Release User"
 git clone https://$4@github.com/$5.git  infra-as-code-repo
+wget https://raw.githubusercontent.com/DenisPalnitsky/gitops-release/master/replace-key.py
 python replace-key.py --file infra-as-code-repo/$1 --key $2 --value $3
 git add .
 git commit -m"Release of key $2 in $1"
