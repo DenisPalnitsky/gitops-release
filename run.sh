5ffc52d9068886bd04aa561883dcb448d929d6f3
Denis Palnitsky <denis@ukama.com>
2021-10-06 16:36:53 +0200

    make github username and imail configurable

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -l
 set -e
 
-git config --global user.email "gitops-release@github.com"
-git config --global user.name "Gitops Release User"
+git config --global user.email  $INPUT_GITHUB_USERNAME 
+git config --global user.name $INPUT_GITHUB_EMAIL
 git clone https://$4@github.com/$5.git  $RUNNER_TEMP/infra-as-code-repo
 wget https://raw.githubusercontent.com/DenisPalnitsky/gitops-release/master/replace-key.py
 python replace-key.py --file $RUNNER_TEMP/infra-as-code-repo/$1 --key $2 --value $3
